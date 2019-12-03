7814bdbed65b0094562856912ac9a90df86ea85d
feelspecial <masakano116@gmail.com>
2019-12-03 15:47:07 +0900

    Categoryテーブル作成を誤ったため削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_062140) do
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "path", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_12_03_064424) do
 
 end
