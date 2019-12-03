8cdb89e5458433516d6d4e88391c13d6fcb16f1b
feelspecial <masakano116@gmail.com>
2019-12-03 17:40:15 +0900

    seedファイルdb作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_065006) do
+ActiveRecord::Schema.define(version: 2019_12_03_082233) do
 
   create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "path", null: false
+    t.integer "path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
