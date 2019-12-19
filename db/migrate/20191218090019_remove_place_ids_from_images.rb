033e6d00c413116263ce814400b8921b8dbd5fc1
feelspecial <masakano116@gmail.com>
2019-12-19 15:32:49 +0900

    テーブルリセット

--- db/migrate/20191218090019_remove_place_ids_from_images.rb
+++
@@ -1,4 +0,0 @@
-class RemovePlaceIdsFromImages < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20191218090514_add_place_id_ref_to_images.rb
+++
@@ -1,4 +0,0 @@
-class AddPlaceIdRefToImages < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
