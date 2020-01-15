e02def05d406d37cd61941332f1e915cc84ba51e
feelspecial <masakano116@gmail.com>
2020-01-15 23:07:21 +0900

    リクエストコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pleces#index"
+  resources :request
   resources :pleces ,only:[:index,:show] do 
     collection do
       get 'search'
